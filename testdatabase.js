9041210e76f7fcc2231072e07c80b54a150565b4
JRSokasits <jsokasits@gmail.com>
2025-08-29 11:41:59 -0400

    Add files via upload
    added test files to repository for final project on GH, most recent versions

---
+++ testdatabase.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        "name" : "Blinky",
+        "avatar" : /imagefolder/image1.png,
+        "firstQuestion" : "I found out about the program from a friend.",
+        "secondQuestion" : "I enjoyed meeting people and sharing the learning experience with them.",
+        "thirdQuestion" : "It is amazing how much can be done with a few lines of code.",
+        "fourthQuestion" : "It was hard but worth the effort.",
+    },
+    {
+        "name" : "EvilScienceBattleDuck",
+        "avatar" : /imagefolder/image2.png,
+        "firstQuestion" : "I found it during an online search.",
+        "secondQuestion" : "It was interesting to see people from so many places.",
+        "thirdQuestion" : ".",
+        "fourthQuestion" :
+    }
+]
---
+++ testserve.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express(); 
+const PORT = 3000;
+
+app.use(express.static(path.join(".", "testpublic")));
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+})
